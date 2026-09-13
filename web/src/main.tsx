98e6256aa7c409cda32254ba15dac6966c3ae394
Zpcin <1585893341@qq.com>
2026-09-13 08:58:03 +0800

    chore: add welcome console log banner
    add a styled startup console banner for the SmartBridge project

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log('%c智触心桥 SmartBridge%c\n方括号千抹两份米饭队 制作', 'font-size:20px;font-weight:900;color:#0e7c86;', 'font-size:13px;color:#5f7482;')
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
